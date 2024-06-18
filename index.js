f770f877bb647a2ecd2f032df3915463a5973575
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-06-18 18:27:04 +0300

    Student data

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-let my_num = 1;
-my_num++;
+const currentYear = 2024
 
-let Student_name = prompt('Введите имя: ');
-let Student_surname = prompt('Введите фамилию: ');
+let student = 'Илья';
+let year = 2006;
+let age = currentYear - year;
 
-let age = 25;
-let height = 178;
+console.log("Студент:", student);
+console.log("Год рождения:", year);
+console.log("Возраст:", age);
 
-console.log('Имя: ', Student_name, ',', 'Фамилия: ', Student_surname, 'Возраст (Лет):', age);Ь
+
+// let my_num = 1;
+// my_num++;
+
+// let Student_name = prompt('Введите имя: ');
+// let Student_surname = prompt('Введите фамилию: ');
+
+// let age = 25;
+// let height = 178;
+
+// console.log('Имя: ', Student_name, ',', 'Фамилия: ', Student_surname, 'Возраст (Лет):', age);
