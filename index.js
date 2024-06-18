3253cae54bb552f6202074ebd65bd38422523c81
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-06-18 18:24:17 +0300

    mini-task

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('First prog')
-window.alert('Hello, world!')
+let my_num = 1;
+my_num++;
+
+let Student_name = prompt('Введите имя: ');
+let Student_surname = prompt('Введите фамилию: ');
+
+let age = 25;
+let height = 178;
+
+console.log('Имя: ', Student_name, ',', 'Фамилия: ', Student_surname, 'Возраст (Лет):', age);Ь
