26a78c45703eae4baa488d3f4c49eb73aae331f3
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-06-18 18:42:19 +0300

    Runners++

--- index.js
+++ index.js
@@ -29,6 +29,31 @@ console.log('Сумма всей покупки: ', product3, '-',  price3 * cou
 
 
 
+let index = 0 // Счетчик
+
+// Бегун 1
+let runner1 = "Денис"
+index++
+console.log(index++, runner1);
+
+// Бегун 2
+let runner2 = "Виктория"
+console.log(index++, runner2);
+
+// Бегун 3
+let runner3 = "Антон"
+console.log(index++, runner3);
+
+// Бегун 4
+let runner4 = "Лена"
+console.log(index++, runner4);
+
+// Бегун 5
+let runner5 = "Никита"
+console.log(index++, runner5);
+
+
+
 
 
 // let my_num = 1;
